93f3cb1510a8f3b9fbea4d9df4cd0700504c7014
mcasimir <maurizio.cas@gmail.com>
2020-11-27 16:33:56 +0100

    wip

--- smoke.js
+++ smoke.js
@@ -1,24 +1,23 @@
-const fetch = require('node-fetch');
+const { Octokit } = require('@octokit/core');
+
+async function dispatch() {
+  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+  const json = await octokit.request('POST /repos/{owner}/{repo}/dispatches', {
+    owner: 'mcasimir',
+    repo: 'kerberos-tests',
+    event_type: 'builds'
+  });
+
+
+  console.log(json);
+}
 
 (async() => {
-  const json = await fetch('https://api.github.com/repos/mcasimir/kerberos-tests/dispatches', {
-    method: 'POST',
-    body:    JSON.stringify({
-      'event_type': 'smoke-tests',
-      'client_payload': {
-        'foo': 'bla'
-      }
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/vnd.github.everest-preview+json'
-    },
-  })
-  .then(res => res.json());
-
-console.log(json)
+  await dispatch();
 })().catch((e) => {
   console.log(e);
   // eslint-disable-next-line no-process-exit
   process.exit(1);
 });
+
