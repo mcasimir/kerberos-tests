a866aae9edf8d1f27db3dbb88e701834297b87fd
mcasimir <maurizio.cas@gmail.com>
2020-11-27 16:44:19 +0100

    test

--- smoke.js
+++ smoke.js
@@ -6,9 +6,17 @@ async function dispatch() {
   const json = await octokit.request('POST /repos/{owner}/{repo}/dispatches', {
     owner: 'mcasimir',
     repo: 'kerberos-tests',
-    event_type: 'builds'
+    event_type: 'builds',
+    client_payload: {
+      urls: {
+        linux_tgz: 'https://downloads.mongodb.com/compass/mongosh-0.5.2-linux.tgz',
+        linux_deb: 'https://downloads.mongodb.com/compass/mongosh_0.5.2_amd64.deb',
+        linux_rpm: 'https://downloads.mongodb.com/compass/mongosh-0.5.2-x86_64.rpm',
+        mac_zip: 'https://downloads.mongodb.com/compass/mongosh-0.5.2-darwin.zip',
+        win_zip: 'https://downloads.mongodb.com/compass/mongosh-0.5.2-win32.zip'
+      }
+    }
   });
-
 
   console.log(json);
 }
